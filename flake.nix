e2d06b2037373fc22e044ef065c50777893705cf
evf <eliseuv816@gmail.com>
2025-08-09 21:50:49 -0300

    Add dependencies

--- flake.nix
+++ flake.nix
@@ -13,18 +13,39 @@
       ...
     }:
     let
-      pkgs = nixpkgs.legacyPackages."x86_64-linux";
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
     in
     {
       devShells.x86_64-linux.default = pkgs.mkShell {
 
-        packages = [
-          (pkgs.python3.withPackages (
+        packages = with pkgs; [
+
+          # CUDA
+          # cudatoolkit
+          # cudaPackages.cudnn
+
+          # Python
+          (python3.withPackages (
             pypkgs: with pypkgs; [
+              # Numerical
               numpy
+              scipy
+              # Machine Learning
+              scikit-learn
+              pytorch
             ]
           ))
         ];
+
+        # shellHook = ''
+        #   export CUDA_PATH=${pkgs.cudatoolkit}
+        #   export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
+        #   export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
+        #   export EXTRA_CCFLAGS="-I/usr/include"
+        # '';
 
       };
 
