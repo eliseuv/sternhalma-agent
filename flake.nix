84a225e4b26280739ff96a39510b8f4f138e9d1c
evf <eliseuv816@gmail.com>
2025-08-30 18:53:46 -0300

    [flake] enable nixpkgs.config.cudaSupport

--- flake.nix
+++ flake.nix
@@ -26,7 +26,10 @@
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        config.allowUnfree = true;
+        config = {
+          allowUnfree = true;
+          cudaSupport = true;
+        };
       };
     in
     {
