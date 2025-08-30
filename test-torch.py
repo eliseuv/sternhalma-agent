8cdbe430d3608ccbea3e7b645fac42273d580aec
evf <eliseuv816@gmail.com>
2025-08-30 18:56:19 -0300

    [torch] update test torch script

--- test-torch.py
+++ test-torch.py
@@ -1,3 +1,5 @@
 import torch
 
 print("CUDA support:", torch.cuda.is_available())
+print("CUDA devices count:", torch.cuda.device_count())
+print("CUDA devices name:", torch.cuda.get_device_name(0))
