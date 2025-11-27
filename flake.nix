102b7820910684f9aead9f1064aa0d4ca7336d10
evf <eliseuv816@gmail.com>
2025-11-27 15:53:10 -0300

    [flake] remove unused CUDA packages and env vars

--- flake.nix
+++ flake.nix
@@ -62,18 +62,8 @@
               ]
             ))
 
-            # CUDA
-            # cudatoolkit
-            # cudaPackages.cudnn
-
           ];
 
-          # shellHook = ''
-          #   export CUDA_PATH=${pkgs.cudatoolkit}
-          #   export LD_LIBRARY_PATH=${pkgs.linuxPackages.nvidia_x11}/lib:${pkgs.ncurses5}/lib
-          #   export EXTRA_LDFLAGS="-L/lib -L${pkgs.linuxPackages.nvidia_x11}/lib"
-          #   export EXTRA_CCFLAGS="-I/usr/include"
-          # '';
 
         };
       }
